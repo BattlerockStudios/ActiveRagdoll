91b533c0ebad635881088c514e267b43066edd8e
Troy Sanders <troyjsanders@gmail.com>
2019-06-09 22:40:41 -0500

    Experimenting with making ragdoll stand up and balance.

---
+++ Assets/Scripts/ActiveRagdoll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveRagdoll : MonoBehaviour
+{
+    [SerializeField]
+    private float force = 10f;
+    public Rigidbody leftLeg;
+    public Rigidbody rightLeg;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            leftLeg.AddForce(transform.up * force, ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            rightLeg.AddForce(transform.up * force, ForceMode.Impulse);
+        }
+    }
+}
